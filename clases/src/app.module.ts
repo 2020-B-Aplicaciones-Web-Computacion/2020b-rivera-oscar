afbc17b2a69841ee6b5574db88eefc737bafa523
ORivera <oscar.rivera@epn.edu.ec>
2021-03-13 22:59:22 -0500

    as

---
+++ clases/src/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import {MascotaModule} from './mascota/mascota.module';
+import {UsuarioModule} from './usuario/usuario.module';
+
+@Module({
+  imports: [
+        MascotaModule,//importamos los modulos creados de mascota y usuario.
+        UsuarioModule
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ clases/src/mascota/mascota.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+
+@Module({
+    imports: [
+
+    ],
+    controllers: [ //controlarodes
+    ],
+    providers: [ //servicios declarados
+    ],
+    exports: [//servicios exportados
+    ],
+})
+
+export class MascotaModule{
+
+}
